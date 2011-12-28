5d3ac476fa522c29a5bd61aae33a9be2a935b32f
Robert Gruendler <robert@dubture.com>
2011-12-28 11:04:00 +0100

    added images and js

---
+++ web/main.js
@@ -0,0 +1,20 @@
+function onBefore() { 
+    $('#feature').fadeOut(300);
+} 
+function onAfter() { 
+    $('#feature').html(this.alt).fadeIn(50);
+}
+
+$(document).ready(function() {
+	
+    $('.slideshow').cycle({
+    		fx: 'scrollLeft',
+            speed:    500, 
+            timeout:  4000,
+            before:  onBefore, 
+            after:   onAfter            
+    	});
+    	
+    $("a.divButton").button();
+	
+});
